51fe8bed814d82e5f47f8ea092c4ff226d750b43
Sai Soumyak Nanda <74289464+sainanda59@users.noreply.github.com>
2021-09-22 16:43:42 +0530

    Update project.js

--- src/components/data/project.js
+++ src/components/data/project.js
@@ -6,7 +6,7 @@ export const ProjectData = [
         about: "I Have Created An Interactive Website Which People Can Use To Sway Away Their Anxiety And Depression And Eliminate Sense Of Inferiority. I Made This Website Interactive With Features Like Baatchit, Diary-Entry, Music, Mood-Tracker, Comics, Etc Which Will Serve To Different Problems.",
         tags: ["HTML", "CSS", "JAVASCRIPT", "REACTJS", "FIREBASE"],
         demo: "https://mere-man-ki-baat.netlify.app/",
-        github: "https://github.com/sainanda59/mere_man_ki_baat",
+        github: "https://github.com/sainanda59/MERE-MANN-KI-BAAT",
         image: "https://github.com/sainanda59/MERE-MANN-KI-BAAT/raw/master/assests/2.png"
 
     },
